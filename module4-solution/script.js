cd567492ef1660194f0176f73d9c59f54b5f3569
Henric Fröberg <henric.froberg@gmail.com>
2020-10-16 21:19:20 +0200

    Solution to module 4

---
+++ module4-solution/script.js
@@ -0,0 +1,15 @@
+(function (window) {
+  names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var name in names) {
+
+    var firstLetter = names[name].toLowerCase().charAt(0);
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[name]);
+    } else {
+      helloSpeaker.speak(names[name]);
+    }
+  }
+
+})(window);
